212520a7ca4283dfa47ff24b527d4eca6a03770f
lcvriend <43742775+lcvriend@users.noreply.github.com>
2023-12-27 14:41:51 +0100

    Update Report class - Rename methods to be less confusing - Add docstrings - Add types - Store settings in "config.toml" - Remove unused imports

--- query/report.py
+++ query/report.py
@@ -1,17 +1,23 @@
-import re
-import json
 from pathlib import Path
-from typing import Any, Callable
+from typing import Callable, NewType
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, pass_context
 from markdown import Markdown
 import pandas as pd
 
+from query.config import CONFIG, get_paths_from_config
 from query.utils import DotDict, TS
-from query.config import get_paths_from_config
 
 
-def render_template(value, context):
+markdownStr = NewType('markdownStr', str)
+htmlStr = NewType('htmlStr', str)
+
+type PostProcessor = Callable[[str], str]
+type PostProcessors = list[Callable[[str], str]]
+type OptionalPostProcessors = PostProcessors | None
+
+
+def render_template(value: str, context: dict) -> str:
     env = Environment()
     template = env.from_string(value)
     rendered = template.render(context)
@@ -19,15 +25,87 @@ def render_template(value, context):
 
 
 class Report:
+    """
+    Class for creating reports from markdown templates.
+
+        Workflow:
+        Input
+            [markdown template]
+            [layout template]
+
+        Step 1: Render markdown as naked html
+        Step 2: Postprocess naked html
+        Step 3: Render naked html into layout
+
+        Output
+            [rendered html]
+
+    The following attributes are exposed to the rendering context:
+    - pd: Pandas.
+    - ts: several timestamps.
+    - meta: container for storing meta information.
+    - sql: container for storing sql queries.
+    - tables: container for storing tables.
+    - charts: container for storing chart specifications.
+
+    Also a `as_template` filter is added to the rendering environment.
+    This allows you to do: `{{ snippet | as_template }}` where `snippet`
+    will be rendered as a template using the context of the main template.
+
+    Attributes:
+    - markdown_template (str): location of markdown template.
+    - layout_template (str): location of layout template.
+    - post (OptionalPostProcessors): postprocessors.
+    - env (Environment): Jinja2 environment for rendering.
+    - meta (DotDict): container for storing meta information.
+    - sql (DotDict): container for storing sql queries.
+    - tables (DotDict): container for storing tables.
+    - charts (DotDict): container for storing chart specifications.
+
+    Methods:
+    - get_rendered_html() -> htmlStr: Render markdown template to layout.
+    - get_naked_html() -> htmlStr: Convert rendered markdown template to html.
+    - get_rendered_markdown() -> htmlStr: Render markdown template to markdown.
+    - to_html(outpath: Path | str) -> None: Save rendered report as 'html'.
+    - to_docx(outpath: Path | str) -> None: Save rendered report as 'docx'.
+    - load_config() -> None: Load settings from CONFIG.
+    """
+
     TEMPLATES: get_paths_from_config('templates')
 
     def __init__(
         self,
-        template_path: str|Path = '',
-        post: Callable|list[Callable]|None = None,
+        markdown_template: str|None = None,
+        layout_template: str|None = None,
+        template_paths: Path|str|list[Path|str]|None = None,
+        post: OptionalPostProcessors = None,
     ):
+        """
+        Parameters:
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+        - layout_template (str|None, optional):
+            Name/location of the layout template.
+        - template_paths (Path|str|list[Path|str]|None, optional):
+            Environment paths (used for locating the templates).
+            By default CONFIG paths and current working directory are added.
+        - post: (OptionalPostProcessors, optional):
+            Postprocessor (or list thereof) to be applied to converted markdown.
+
+        Returns:
+        None
+        """
+        self.markdown_template = markdown_template
+        self.layout_template = layout_template
+
+        if isinstance(template_paths, (str, Path)):
+            template_paths = [template_paths]
+        elif template_paths is None:
+            template_paths = []
+
         template_paths = [
-            template_path,
+            *template_paths,
+            '.',
             *self.TEMPLATES,
         ]
         self.env = Environment(
@@ -46,7 +124,6 @@ class Report:
         else:
             raise TypeError("Post is not of type `Callable` or `list`")
 
-        from jinja2 import pass_context
         @pass_context
         def as_template(context, value):
             tpl = self.env.from_string(value)
@@ -104,13 +181,39 @@ class Report:
     def charts(self, value) -> None:
         raise AttributeError("Cannot overwrite namespace.")
 
-    def render_to_layout(
+    def get_rendered_html(
         self,
-        path_to_markdown: str|Path,
-        path_to_template: str = 'report.html',
-        post = None,
+        markdown_template: str|None = None,
+        layout_template: str|None = None,
+        post: OptionalPostProcessors = None,
         **kwargs
-    ) -> str:
+    ) -> htmlStr:
+        """
+        Renders a markdown template to a html string:
+        1. the template gets rendered to a markdown string.
+        2. the markdown string is converted to html.
+        3. the converted string is (optionally) run through post-processing.
+        4. the converted html is rendered into the layout template.
+
+        Parameters:
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+            Defaults to the `markdown_template` set at initialization.
+        - layout_template (str|None, optional):
+            Name/location of the layout template.
+            Defaults to the `layout_template` set at initialization.
+        - post: (OptionalPostProcessors, optional):
+            Postprocessor (or list thereof) to be applied to converted markdown.
+        - **kwargs: Additional keyword arguments to pass to the template.
+
+        Returns:
+        htmlStr: Rendered html content.
+        """
+        if markdown_template is None:
+            markdown_template = self.markdown_template
+        if layout_template is None:
+            layout_template = self.layout_template
+
         if post is None:
             pp = self.post.copy()
         elif post == False:
@@ -120,70 +223,139 @@ class Report:
         elif isinstance(post, list):
             pp = [*self.post, *post]
 
-        tpl = self.env.get_template(path_to_template)
-        content = self.render_content_as_raw_html(path_to_markdown, **kwargs)
+        tpl = self.env.get_template(layout_template)
+        naked_html = self.get_naked_html(markdown_template, **kwargs)
+
         for postprocessor in pp:
-            content = postprocessor(content)
-        as_html = tpl.render(
+            content = postprocessor(naked_html)
+        html = tpl.render(
             content=content,
             toc=self.markdown.toc_tokens,
             **kwargs
         )
-        return as_html
+        return html
 
-    def render_content_as_md(
+    def get_naked_html(
         self,
-        path_to_markdown: str|Path,
+        markdown_template: str|None = None,
         **kwargs
-    ) -> str:
-        tpl = self.env.get_template(path_to_markdown)
-        as_markdown = tpl.render(**kwargs)
-        return as_markdown
+    ) -> htmlStr:
+        """
+        Renders a markdown template to a html string.
+        First, the template gets rendered to a markdown string.
+        Then the markdown string is converted to html.
 
-    def render_content_as_raw_html(
+        Parameters:
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+            Defaults to the `markdown_template` set at initialization.
+        - **kwargs: Additional keyword arguments to pass to the template.
+
+        Returns:
+        htmlStr: Rendered html content.
+        """
+        if markdown_template is None:
+            markdown_template = self.markdown_template
+
+        markdown = self.get_rendered_markdown(markdown_template, **kwargs)
+        naked_html = self.markdown.convert(markdown)
+        return naked_html
+
+    def get_rendered_markdown(
         self,
-        path_to_markdown: str|Path,
+        markdown_template: str|None = None,
         **kwargs
-    ) -> str:
-        as_markdown = self.render_content_as_md(path_to_markdown, **kwargs)
-        as_html = self.markdown.convert(as_markdown)
-        return as_html
+    ) -> markdownStr:
+        """
+        Renders a markdown template to a markdown string.
+
+        Parameters:
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+            Defaults to the `markdown_template` set at initialization.
+        - **kwargs: Additional keyword arguments to pass to the template.
+
+        Returns:
+        markdownStr: Rendered markdown content.
+        """
+        if markdown_template is None:
+            markdown_template = self.markdown_template
+
+        markdown_template = self.env.get_template(markdown_template)
+        rendered_markdown = markdown_template.render(**kwargs)
+        return rendered_markdown
 
     def to_html(
         self,
-        source: str|Path,
-        output_path: str|Path,
-        path_to_template: str|Path = 'report.html',
-        post = None,
+        output_path: Path|str,
+        markdown_template: str|None = None,
+        layout_template: str|None = None,
+        post: OptionalPostProcessors = None,
         **kwargs
     ) -> None:
-        as_html = self.render_to_layout(
-            source,
-            path_to_template,
-            post=post,
+        """
+        Render report to html.
+
+        Parameters:
+        - output_path (Path|str): Filepath to store rendered report.
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+            Defaults to the `markdown_template` set at initialization.
+        - layout_template (str|None, optional):
+            Name/location of the layout template.
+            Defaults to the `layout_template` set at initialization.
+        - post: (OptionalPostProcessors, optional):
+            Postprocessor (or list thereof) to be applied to converted markdown.
+        - **kwargs: Additional keyword arguments to pass to the template.
+
+        Returns:
+        None
+        """
+        html = self.get_rendered_html(
+            markdown_template,
+            layout_template,
+            post = post,
             **kwargs
         )
-        Path(output_path).write_text(as_html)
+        Path(output_path).write_text(html)
 
     def to_docx(
         self,
-        source: str|Path,
-        output_path: str|Path,
-        path_to_template: str|Path = 'report.html',
-        post = None,
+        output_path: Path|str,
+        markdown_template: str|None = None,
+        layout_template: str|None = None,
+        post: OptionalPostProcessors = None,
         **kwargs
     ) -> None:
+        """
+        Render report to docx.
+
+        Parameters:
+        - output_path (Path|str): Filepath to store rendered report.
+        - markdown_template (str|None, optional):
+            Name/location of the markdown template.
+            Defaults to the `markdown_template` set at initialization.
+        - layout_template (str|None, optional):
+            Name/location of the layout template.
+            Defaults to the `layout_template` set at initialization.
+        - post: (OptionalPostProcessors, optional):
+            Postprocessor (or list thereof) to be applied to converted markdown.
+        - **kwargs: Additional keyword arguments to pass to the template.
+
+        Returns:
+        None
+        """
         import subprocess
         output_path = Path(output_path).resolve()
         temp_path = output_path.with_suffix('.temp.html')
 
-        as_html = self.render_to_layout(
-            source,
-            path_to_template,
-            post=post,
+        html = self.get_rendered_html(
+            markdown_template,
+            layout_template,
+            post = post,
             **kwargs
         )
-        temp_path.write_text(as_html)
+        temp_path.write_text(html)
         args = ['pandoc', str(temp_path), '-o', str(output_path)]
 
         r = subprocess.run(args, capture_output=True)
@@ -191,8 +363,7 @@ class Report:
             print(error)
         temp_path.unlink()
 
-    @classmethod
-    def load_config(cls) -> None:
-        conf = json.loads((cls.TEMPLATES / 'config.json').read_text())
-        for pat, val in conf.items():
+    @staticmethod
+    def load_config() -> None:
+        for pat, val in CONFIG['report']['styler'].items():
             pd.set_option(pat, val)
