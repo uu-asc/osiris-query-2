588cd8f19708deeec2bc916b751333490579ef02
lcvriend <43742775+lcvriend@users.noreply.github.com>
2024-11-06 22:53:04 +0100

    Add optional imports

--- query/__init__.py
+++ query/__init__.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+try:
+    import flatbread
+    import xquery
+    import key_extractor
+except ImportError:
+    ...
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.show_dimensions', True)
