d07dfc2fd781f43c33fb5fcec1b3489a81a521ce
lcvriend <43742775+lcvriend@users.noreply.github.com>
2024-11-25 13:40:29 +0100

    Add all files/folders in definitions

---
+++ definitions/sanity/last_mutation.sql
@@ -0,0 +1,13 @@
+with max_mutation_date as (
+    select max({{ mutation_date_column }}) max_mutation_date from {{ table }}
+)
+
+select
+    max_mutation_date,
+    (sysdate - max_mutation_date) / 24 n_hours,
+    {{ threshold_in_hours }} threshold_in_hours,
+    case
+        when ((sysdate - max_mutation_date) / 24) < {{ threshold_in_hours }} then 'Y'
+        else 'N'
+    end below_threshold
+from max_mutation_date
